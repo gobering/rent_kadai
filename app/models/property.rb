e8e522b91f4bfe93aea39dc89bdeab881f62adc6
sho ebisui <raphi.jus1412@gmail.com>
2022-10-18 16:38:47 +0900

    最寄り駅のフォーマットを作成した

--- app/models/property.rb
+++ app/models/property.rb
@@ -1,4 +1,10 @@
 class Property < ApplicationRecord
   has_many :nearest_stations, dependent: :destroy
   accepts_nested_attributes_for :nearest_stations, allow_destroy: true
+
+  validates :name, presence: true
+  validates :price, presence: true
+  validates :address, presence: true
+  validates :age, presence: true
+  validates :remarks, presence: true
 end
