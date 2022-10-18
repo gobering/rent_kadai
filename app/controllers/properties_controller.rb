228b56749a57a03a8ce5bfc981fc352c95c5474a
sho ebisui <raphi.jus1412@gmail.com>
2022-10-18 15:30:32 +0900

    物件のCRUD機能作成、最寄り駅のmodelとtableを作ってアソシエーションさせた

---
+++ app/controllers/properties_controller.rb
@@ -0,0 +1,55 @@
+class PropertiesController < ApplicationController
+  before_action :set_property, only: %i[ show edit update destroy ]
+
+  def index
+    @properties = Property.all
+  end
+
+  def show
+  end
+
+  def new
+    @property = Property.new
+    2.times { @property.nearest_stations.build }
+  end
+
+  def edit
+  end
+
+  def create
+    @property = Property.new(property_params)
+    if @property.save
+      redirect_to properties_path, notice: "物件を登録しました！"
+    else
+      render :new
+    end
+  end
+
+  def update
+    if @property.update(property_params)
+      redirect_to properties_path, notice: "物件を編集しました！"
+    else
+      render :edit
+    end
+  end
+
+  def destroy
+    @property.destroy
+    respond_to do |format|
+      format.html { redirect_to properties_url, notice: "物件を削除しました！" }
+      format.json { head :no_content }
+    end
+  end
+
+  private
+    
+  def set_property
+    @property = Property.find(params[:id])
+  end
+
+  def property_params
+    params.require(:property).permit(:name, :price, :address, :age, :remarks, 
+    nearest_stations_attributes: [:route, :station, :minutes_walk, :property_id, :id, :_destroy ],)
+  end
+
+end
