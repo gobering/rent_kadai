4bbb16f504d2b1b20a99532f1952c2636bc70f05
sho ebisui <raphi.jus1412@gmail.com>
2022-10-18 17:54:31 +0900

    最寄り駅のバリデーションの変更

--- app/models/nearest_station.rb
+++ app/models/nearest_station.rb
@@ -1,6 +1,3 @@
 class NearestStation < ApplicationRecord
   belongs_to :property, inverse_of: :nearest_stations
-  validates :route_name, presence: true
-  validates :station_name, presence: true
-  validates :time, presence: true
 end
